cbbd65e1504844a8931f51c730ba6e685b85ac0e
shivsarthak <shivsarthak34@gmail.com>
2020-09-16 23:57:40 +0530

    bot added

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,25 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="au")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-@bot.event
-async def on_ready():
-    print("Logged in as {}({})".format(bot.user.name, bot.user.id))
+@client.command()
+async def muteAll(ctx):
+    channel = client.get_channel(ctx.author.voice.channel.id)
+    members = list(channel.members)
+    for usr in members:
+        await ctx.send(f"```[*] muting {usr.nick}```")
+        await usr.edit(mute=True)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+
+@client.command()
+async def unmuteAll(ctx):
+    channel = client.get_channel(ctx.author.voice.channel.id)
+    members = list(channel.members)
+    for usr in members:
+        await ctx.send(f"```[*] unmuting {usr.nick}```")
+        await usr.edit(mute=False)
 
 if __name__ == "__main__":
-    bot.run(TOKEN)
+    client.run(TOKEN)
