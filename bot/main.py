46e9e6ac88c6cd3b3fcddfc7c9acfeb0842babbb
shivsarthak <shivsarthak34@gmail.com>
2020-09-17 00:05:29 +0530

    change key

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="au")
+client = commands.Bot(command_prefix="~")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @client.command()
